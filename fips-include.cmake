9ecc17dee6fbc6dbe7ef912eba1f9fd586d7f76b
Everton Fernando Patitucci da Silva <patucci@gmail.com>
2017-02-23 22:22:37 -0500

    Add minimum working fips setup
    a fips static lib was added to be able to have the slmem files as a single
    entry in the generated project file, to make that work, a stub file was
    created that does nothing but include the slmem.h header.
    
    added tests group and renamed main.cpp to test0.cpp, I'll revisit it in a
    further change to make the test within more uniform.
    
    fixed a warning in alloc_debug when compiling for x64

---
+++ fips-include.cmake
@@ -0,0 +1,3 @@
+set(SLMEM_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+fips_include_directories("${SLMEM_DIR}/include")
---
+++ stub.cpp
@@ -0,0 +1,5 @@
+#include "slmem.h"
+
+// this file only exists to be able to create a cmake static lib
+// which shouldn't be linked by any other project, but will at least
+// be visible in the generated IDE files (only tested with Visual Studio 2015)
